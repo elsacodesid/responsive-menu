6aef509147c20b6ca499dbf6d2a530be7d332d56
elsacodesid <elsacodesid@gmail.com>
2024-03-25 10:00:07 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.querySelector(".toggle_btn")
+const toggleBtnIcon = document.querySelector(".toggle_btn i")
+const dropDownMenu = document.querySelector(".dropdown_menu")
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle("open")
+    const isOpen = dropDownMenu.classList.contains("open")
+
+    toggleBtnIcon.className = isOpen ? "fas fa-times" : "fas fa-bars"
+
+}
